83f02b6eadea9b5235b3bad68e696cf66c63058f
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-01 18:21:07 +0100

    Removed Class1 and added Category, Transation classes and ETransationType enum
    This commit removes redundant Class1.cs file from the Fina.Core directory. New classes named Category.cs and Transation.cs have been included under the Models directory, specifying properties for each model. In addition, a new enumeration ETransationType.cs has been added in the Enums directory providing transaction types.

---
+++ Fina.Core/Enums/ETransationType.cs
@@ -0,0 +1,7 @@
+namespace Fina.Core.Enums;
+
+public enum ETransationType
+{
+    Deposit=1,
+    Withdraw=2
+}
---
+++ Fina.Core/Models/Transation.cs
@@ -0,0 +1,15 @@
+using Fina.Core.Enums;
+
+namespace Fina.Core.Models;
+
+public class Transation
+{
+    public long Id { get; set; }
+    public string Title { get; set; }=String.Empty;
+
+    public DateTime CreatedAt { get; set; }=DateTime.Now;
+    public DateTime? PaidOrreceivedAt { get; set; }
+
+    public ETransationType Type { get; set; } = ETransationType.Withdraw;
+    public long CategoryId { get; set; }
+}
