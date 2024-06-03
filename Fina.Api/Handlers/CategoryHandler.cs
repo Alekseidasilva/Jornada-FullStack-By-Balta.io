a3e056f2d9af02e2156f242b84dc9fb067ae11e4
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-03 07:37:41 +0100

    :art: Add Transation Methods

--- Fina.Api/Handlers/CategoryHandler.cs
+++ Fina.Api/Handlers/CategoryHandler.cs
@@ -3,6 +3,7 @@ using Fina.Core.Handlers;
 using Fina.Core.Models;
 using Fina.Core.Requests.Categories;
 using Fina.Core.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fina.Api.Handlers;
 
@@ -25,7 +26,7 @@ public class CategoryHandler(AppDbContext context):ICategoryHandler
         }
         catch (Exception e)//Pode fazer tratamento de exceptiosn de formais ricas usando dbexceptions
         {
-            //Serilog ou Telemetry
+            // Aqui pode usar tb o Serilog ou Telemetry
             
             return new Response<Category?>(null, 500, "Nao foi possivel criar a categoria!");
         }
@@ -33,21 +34,103 @@ public class CategoryHandler(AppDbContext context):ICategoryHandler
 
     public async Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
     {
-        throw new NotImplementedException();
+       //Primeiro fazer o processo de reidratacao
+       //que e obter a a categoria do Banco e ver se ela existe
+       try
+       {
+
+           var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == request.Id
+               && x.UserId==request.UserId, CancellationToken.None);
+           if (category is null)
+               return new Response<Category?>(null, 404, "Categoria nao encontrada");
+           category.Title = request.Title;
+           category.Description = request.Description;
+
+           context.Categories.Update(category);
+           await context.SaveChangesAsync();
+
+           return new Response<Category?>(category, message: "Categoria atualizada com Sucesso!");
+
+       }
+       catch (Exception e)
+       {
+           return new Response<Category?>(null,500,  "Nao foi Possivel actualizar a cattegoria!");
+       }
     }
 
     public async Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => 
+                x.Id == request.Id && 
+                x.UserId==request.UserId, CancellationToken.None);
+            if (category is null)
+                return new Response<Category?>(null, 404, "Categoria nao encontrada");
+            
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return new Response<Category?>(category, message: "Categoria removida com Sucesso!");
+
+        }
+        catch (Exception e)
+        {
+            return new Response<Category?>(null,500,  "Nao foi Possivel remover a cattegorio!");
+        }
     }
 
     public async Task<Response<Category?>> GetByIdAsync(GetByIdCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await context
+                .Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x
+                                              .Id == request.Id && x
+                    .UserId==request.UserId, CancellationToken.None);
+
+            return category is null?
+                new Response<Category?>(null, 404, "Categoria nao encontrada")
+                :new Response<Category?>(category);
+            
+            
+        }
+        catch (Exception e)
+        {
+            return new Response<Category?>(null,500,  "Nao foi Possivel recuperar a cattegorio!");
+        }
     }
 
     public async Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            //Count
+            var query = context
+                .Categories
+                .AsNoTracking()
+                .Where(x => x.UserId == request.UserId)
+                .OrderBy(x => x.Title);
+
+            var categories = await query
+                .Skip(request.PageNumber * request.PageSize) //Paginacao
+                .Take(request.PageSize)
+                .ToListAsync();
+
+            var count = await query.CountAsync();
+
+            return new PagedResponse<List<Category>?>(
+                categories,
+                count,
+                request.PageNumber,
+                request.PageSize
+            );
+        }
+        catch (Exception e)
+        {
+            return new PagedResponse<List<Category>?>(null,500,  "Nao foi Possivel recuperar a cattegorio!");
+        }
     }
 }
--- Fina.Api/Handlers/TransationHandler.cs
+++ Fina.Api/Handlers/TransationHandler.cs
@@ -1,4 +1,5 @@
 using Fina.Api.Data;
+using Fina.Core.Enums;
 using Fina.Core.Handlers;
 using Fina.Core.Models;
 using Fina.Core.Requests.Transations;
@@ -10,7 +11,29 @@ public class TransationHandler(AppDbContext context):ITransationHandler
 {
     public async Task<Response<Transation?>> CreateAsync(CreateTransationRequest request)
     {
-        throw new NotImplementedException();
+        if (request is { Type: ETransationType.Withdraw, Amonut:>=0})
+            request.Amonut *= -1;
+        try
+        {
+            var transation = new Transation
+            {
+               UserId = request.UserId,
+               CategoryId = request.CategoryId,
+               CreatedAt = DateTime.Now,
+               Amonut = request.Amonut,
+               PaidOrreceivedAt = request.PaidOrReceivedAt,
+               Title = request.Title,
+               Type = request.Type
+            };
+            await context.Transations.AddAsync(transation);
+            await context.SaveChangesAsync();
+
+            return new Response<Transation?>(transation, 201,"Transacao criada com Sucesso!");
+        }
+        catch (Exception e)
+        {
+            return new Response<Transation?>(null, 500,"Nao foi possivel criar a transacao");
+        }
     }
 
     public async Task<Response<Transation?>> UpdateAsync(UpdateTransationRequest request)
